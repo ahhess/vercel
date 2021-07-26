648b71d87ef735de14bc7266bd21197cc50e6535
Andreas <andreas.h.hess@googlemail.com>
2021-07-27 01:00:02 +0200

    heroes sorting by likes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,16 +19,9 @@ export class DashboardComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => {
-        // this.heroes = heroes.slice(1, 5)
         this.heroes = heroes;
-        // heroes.map(hero => hero.likes).sort().reduce(this.heroes.push(hero));
-        this.heroes.sort(hero => hero.likes);
-        const l = heroes.length;
-        var s = 0;
-        if (l >= 4) {
-          s = l - 4;
-        }
-        this.heroes = this.heroes.slice(s, l);
+        this.heroes.sort((a, b) => b.likes - a.likes);
+        this.heroes = heroes.slice(1, 5);
       });
   }
 }
